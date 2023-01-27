565b8f54c9372a49ca5df7014f92a3a12c62a71b
RHRolun <rlundber@redhat.com>
2023-01-27 17:22:24 +0100

    fix

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -32,7 +32,7 @@ with DAG(
     # res
     test = KubernetesPodOperator(
         namespace="airflow",
-        image="alpine",
+        image="quay.io/apache/bash",
         cmds=["bash", "-cx"],
         arguments=["echo", "10", "echo pwd"],
         name="test",
