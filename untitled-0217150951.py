1ab4bfa66a8dc8bc91e26f2610cfc69d54851f24
RHRolun <113337503+RHRolun@users.noreply.github.com>
2023-02-17 16:09:52 +0100

    Pushed DAG untitled-0217150951

---
+++ untitled-0217150951.py
@@ -0,0 +1,76 @@
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.kubernetes.volume import Volume
+from airflow.kubernetes.volume_mount import VolumeMount
+import datetime
+import os
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+
+from airflow.kubernetes.secret import Secret
+
+
+args = {
+    "project_id": "untitled-0217150951",
+}
+
+
+dag = DAG(
+    "untitled-0217150951",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="""
+Created with Elyra 3.12.0 pipeline editor using `untitled.pipeline`.
+    """,
+    is_paused_upon_creation=False,
+)
+
+
+# Ensure that the secret named 'airflow-storage' is defined in the Kubernetes namespace where this pipeline will be run
+env_var_secret_id = Secret(
+    deploy_type="env",
+    deploy_target="AWS_ACCESS_KEY_ID",
+    secret="airflow-storage",
+    key="AWS_ACCESS_KEY_ID",
+)
+env_var_secret_key = Secret(
+    deploy_type="env",
+    deploy_target="AWS_SECRET_ACCESS_KEY",
+    secret="airflow-storage",
+    key="AWS_SECRET_ACCESS_KEY",
+)
+
+
+# Operator source: telecom-customer-churn-airflow/include/notebooks/process_data-Copy1.ipynb
+
+op_a2d46e75_862b_45c5_961a_1a7adb4ce662 = KubernetesPodOperator(
+    name="process_data_Copy1",
+    trigger_rule="all_success",
+    namespace="airflow",
+    image="quay.io/eformat/airflow-runner:2.5.1",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading file:///opt/app-root/lib/python3.8/site-packages/elyra/kfp/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L file:///opt/app-root/lib/python3.8/site-packages/elyra/kfp/bootstrapper.py --output bootstrapper.py && echo 'Downloading file:///elyra-deps/requirements-elyra.txt' && echo 'Downloading file:///elyra-deps/requirements-elyra-py37.txt' && curl --fail -H 'Cache-Control: no-cache' -L file:///elyra-deps/requirements-elyra-py37.txt --output requirements-elyra-py37.txt && curl --fail -H 'Cache-Control: no-cache' -L file:///elyra-deps/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'untitled' --cos-endpoint http://s3.openshift-storage.svc --cos-bucket airflow-storage-6ddf8b2b-517b-4511-84bc-58ebbbbaf809 --cos-directory 'untitled-0217150951' --cos-dependencies-archive 'process_data-Copy1-a2d46e75-862b-45c5-961a-1a7adb4ce662.tar.gz' --file 'telecom-customer-churn-airflow/include/notebooks/process_data-Copy1.ipynb' "
+    ],
+    task_id="process_data_Copy1",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "untitled-{{ ts_nodash }}",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[
+        Secret("env", "AWS_ACCESS_KEY_ID", "airflow-storage", "AWS_ACCESS_KEY_ID"),
+        Secret(
+            "env", "AWS_SECRET_ACCESS_KEY", "airflow-storage", "AWS_SECRET_ACCESS_KEY"
+        ),
+    ],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
