7df392d1b5482559a31e47791839f59fa105f7da
RHRolun <113337503+RHRolun@users.noreply.github.com>
2023-02-17 13:59:40 +0100

    Pushed DAG train_and_compare_models-0217125938

---
+++ train_and_compare_models-0217125938.py
@@ -0,0 +1,186 @@
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.kubernetes.volume import Volume
+from airflow.kubernetes.volume_mount import VolumeMount
+import datetime
+import os
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+
+from airflow.kubernetes.secret import Secret
+
+
+args = {
+    "project_id": "train_and_compare_models-0217125938",
+}
+
+
+dag = DAG(
+    "train_and_compare_models-0217125938",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="""
+Created with Elyra 3.12.0 pipeline editor using `train_and_compare_models.pipeline`.
+    """,
+    is_paused_upon_creation=False,
+)
+
+
+# Ensure that the secret named 'airflow-storage' is defined in the Kubernetes namespace where this pipeline will be run
+env_var_secret_id = Secret(
+    deploy_type="env",
+    deploy_target="AWS_ACCESS_KEY_ID",
+    secret="airflow-storage",
+    key="AWS_ACCESS_KEY_ID",
+)
+env_var_secret_key = Secret(
+    deploy_type="env",
+    deploy_target="AWS_SECRET_ACCESS_KEY",
+    secret="airflow-storage",
+    key="AWS_SECRET_ACCESS_KEY",
+)
+
+
+# Operator source: telecom-customer-churn-airflow/include/notebooks/process_data.ipynb
+
+op_9ca028c1_8616_4fee_971f_f8225b23a7c9 = KubernetesPodOperator(
+    name="process_data",
+    trigger_rule="all_success",
+    namespace="airflow",
+    image="quay.io/eformat/airflow-runner:2.5.1",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading file:///opt/app-root/lib/python3.8/site-packages/elyra/kfp/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L file:///opt/app-root/lib/python3.8/site-packages/elyra/kfp/bootstrapper.py --output bootstrapper.py && echo 'Downloading file:///elyra-deps/requirements-elyra.txt' && echo 'Downloading file:///elyra-deps/requirements-elyra-py37.txt' && curl --fail -H 'Cache-Control: no-cache' -L file:///elyra-deps/requirements-elyra-py37.txt --output requirements-elyra-py37.txt && curl --fail -H 'Cache-Control: no-cache' -L file:///elyra-deps/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'train_and_compare_models' --cos-endpoint http://s3.openshift-storage.svc --cos-bucket airflow-storage-6ddf8b2b-517b-4511-84bc-58ebbbbaf809 --cos-directory 'train_and_compare_models-0217125938' --cos-dependencies-archive 'process_data-9ca028c1-8616-4fee-971f-f8225b23a7c9.tar.gz' --file 'telecom-customer-churn-airflow/include/notebooks/process_data.ipynb' "
+    ],
+    task_id="process_data",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "train_and_compare_models-{{ ts_nodash }}",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[
+        Secret("env", "AWS_ACCESS_KEY_ID", "airflow-storage", "AWS_ACCESS_KEY_ID"),
+        Secret(
+            "env", "AWS_SECRET_ACCESS_KEY", "airflow-storage", "AWS_SECRET_ACCESS_KEY"
+        ),
+    ],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
+
+
+# Operator source: telecom-customer-churn-airflow/include/notebooks/model_xgboost.ipynb
+
+op_6bd0688e_1b5d_4190_8683_66f0e4e09c98 = KubernetesPodOperator(
+    name="model_xgboost",
+    trigger_rule="all_success",
+    namespace="airflow",
+    image="quay.io/eformat/airflow-runner:2.5.1",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading file:///opt/app-root/lib/python3.8/site-packages/elyra/kfp/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L file:///opt/app-root/lib/python3.8/site-packages/elyra/kfp/bootstrapper.py --output bootstrapper.py && echo 'Downloading file:///elyra-deps/requirements-elyra.txt' && echo 'Downloading file:///elyra-deps/requirements-elyra-py37.txt' && curl --fail -H 'Cache-Control: no-cache' -L file:///elyra-deps/requirements-elyra-py37.txt --output requirements-elyra-py37.txt && curl --fail -H 'Cache-Control: no-cache' -L file:///elyra-deps/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'train_and_compare_models' --cos-endpoint http://s3.openshift-storage.svc --cos-bucket airflow-storage-6ddf8b2b-517b-4511-84bc-58ebbbbaf809 --cos-directory 'train_and_compare_models-0217125938' --cos-dependencies-archive 'model_xgboost-6bd0688e-1b5d-4190-8683-66f0e4e09c98.tar.gz' --file 'telecom-customer-churn-airflow/include/notebooks/model_xgboost.ipynb' "
+    ],
+    task_id="model_xgboost",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "train_and_compare_models-{{ ts_nodash }}",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[
+        Secret("env", "AWS_ACCESS_KEY_ID", "airflow-storage", "AWS_ACCESS_KEY_ID"),
+        Secret(
+            "env", "AWS_SECRET_ACCESS_KEY", "airflow-storage", "AWS_SECRET_ACCESS_KEY"
+        ),
+    ],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
+
+op_6bd0688e_1b5d_4190_8683_66f0e4e09c98 << op_9ca028c1_8616_4fee_971f_f8225b23a7c9
+
+
+# Operator source: telecom-customer-churn-airflow/include/notebooks/model_randomforest.ipynb
+
+op_819bd5cd_a021_4ce6_80b4_fc119da9fb73 = KubernetesPodOperator(
+    name="model_randomforest",
+    trigger_rule="all_success",
+    namespace="airflow",
+    image="quay.io/eformat/airflow-runner:2.5.1",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading file:///opt/app-root/lib/python3.8/site-packages/elyra/kfp/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L file:///opt/app-root/lib/python3.8/site-packages/elyra/kfp/bootstrapper.py --output bootstrapper.py && echo 'Downloading file:///elyra-deps/requirements-elyra.txt' && echo 'Downloading file:///elyra-deps/requirements-elyra-py37.txt' && curl --fail -H 'Cache-Control: no-cache' -L file:///elyra-deps/requirements-elyra-py37.txt --output requirements-elyra-py37.txt && curl --fail -H 'Cache-Control: no-cache' -L file:///elyra-deps/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'train_and_compare_models' --cos-endpoint http://s3.openshift-storage.svc --cos-bucket airflow-storage-6ddf8b2b-517b-4511-84bc-58ebbbbaf809 --cos-directory 'train_and_compare_models-0217125938' --cos-dependencies-archive 'model_randomforest-819bd5cd-a021-4ce6-80b4-fc119da9fb73.tar.gz' --file 'telecom-customer-churn-airflow/include/notebooks/model_randomforest.ipynb' "
+    ],
+    task_id="model_randomforest",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "train_and_compare_models-{{ ts_nodash }}",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[
+        Secret("env", "AWS_ACCESS_KEY_ID", "airflow-storage", "AWS_ACCESS_KEY_ID"),
+        Secret(
+            "env", "AWS_SECRET_ACCESS_KEY", "airflow-storage", "AWS_SECRET_ACCESS_KEY"
+        ),
+    ],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
+
+op_819bd5cd_a021_4ce6_80b4_fc119da9fb73 << op_9ca028c1_8616_4fee_971f_f8225b23a7c9
+
+
+# Operator source: telecom-customer-churn-airflow/include/notebooks/compare_and_push.ipynb
+
+op_5c7a887a_6833_4b0e_8795_e0b8bbd6d2b9 = KubernetesPodOperator(
+    name="compare_and_push",
+    trigger_rule="all_success",
+    namespace="airflow",
+    image="quay.io/eformat/airflow-runner:2.5.1",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading file:///opt/app-root/lib/python3.8/site-packages/elyra/kfp/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L file:///opt/app-root/lib/python3.8/site-packages/elyra/kfp/bootstrapper.py --output bootstrapper.py && echo 'Downloading file:///elyra-deps/requirements-elyra.txt' && echo 'Downloading file:///elyra-deps/requirements-elyra-py37.txt' && curl --fail -H 'Cache-Control: no-cache' -L file:///elyra-deps/requirements-elyra-py37.txt --output requirements-elyra-py37.txt && curl --fail -H 'Cache-Control: no-cache' -L file:///elyra-deps/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'train_and_compare_models' --cos-endpoint http://s3.openshift-storage.svc --cos-bucket airflow-storage-6ddf8b2b-517b-4511-84bc-58ebbbbaf809 --cos-directory 'train_and_compare_models-0217125938' --cos-dependencies-archive 'compare_and_push-5c7a887a-6833-4b0e-8795-e0b8bbd6d2b9.tar.gz' --file 'telecom-customer-churn-airflow/include/notebooks/compare_and_push.ipynb' "
+    ],
+    task_id="compare_and_push",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "train_and_compare_models-{{ ts_nodash }}",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[
+        Secret("env", "AWS_ACCESS_KEY_ID", "airflow-storage", "AWS_ACCESS_KEY_ID"),
+        Secret(
+            "env", "AWS_SECRET_ACCESS_KEY", "airflow-storage", "AWS_SECRET_ACCESS_KEY"
+        ),
+    ],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
+
+op_5c7a887a_6833_4b0e_8795_e0b8bbd6d2b9 << op_6bd0688e_1b5d_4190_8683_66f0e4e09c98
+
+op_5c7a887a_6833_4b0e_8795_e0b8bbd6d2b9 << op_819bd5cd_a021_4ce6_80b4_fc119da9fb73
