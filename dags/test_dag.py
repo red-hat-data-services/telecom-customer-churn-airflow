f7be46ed236f7fca9014512b97a908d345f52c25
RHRolun <rlundber@redhat.com>
2023-02-20 11:24:10 +0100

    Add a test DAG again

---
+++ dags/test_dag.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.python import PythonOperator
+
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+
+with DAG(
+    "TestDAG",
+    default_args={
+        "depends_on_past": False,
+        "retries": 0,
+        "retry_delay": timedelta(minutes=5),
+    },
+    description="A simple tutorial DAG",
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+) as dag:
+
+    test = KubernetesPodOperator(
+        namespace="airflow",
+        image="quay.io/apache/bash",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10", "echo pwd"],
+        name="test",
+        task_id="test",
+        get_logs=True,
+    )
+
+    test
