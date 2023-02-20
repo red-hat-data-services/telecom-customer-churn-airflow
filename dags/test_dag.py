5261ba7e6f6f2eaf871beb0ab49d9235a40272fb
RHRolun <rlundber@redhat.com>
2023-02-20 12:01:52 +0100

    Revert test

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,8 +1,6 @@
 from airflow import DAG
 from airflow.decorators import task
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.providers.papermill.operators.papermill import PapermillOperator
-from airflow.operators.python import PythonOperator
 
 from datetime import datetime, timedelta
 from textwrap import dedent
@@ -19,21 +17,14 @@ with DAG(
     start_date=datetime(2021, 1, 1),
     catchup=False,
 ) as dag:
-    PapermillOperator(
-        task_id="run_example_notebook",
-        input_nb="../include/notebooks/hello_world.ipynb",
-        output_nb="../include/notebooks/tmp/out-{{ execution_date }}.ipynb",
-        parameters={"msgs": "Ran from Airflow at {{ execution_date }}!"},
+    test = KubernetesPodOperator(
+        namespace="airflow",
+        image="quay.io/eformat/airflow-runner:2.5.1",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10", "echo pwd"],
+        name="test",
+        task_id="test",
+        get_logs=True,
     )
 
-    # test = KubernetesPodOperator(
-    #     namespace="airflow",
-    #     image="quay.io/eformat/airflow-runner:2.5.1",
-    #     cmds=["bash", "-cx"],
-    #     arguments=["echo", "10", "echo pwd"],
-    #     name="test",
-    #     task_id="test",
-    #     get_logs=True,
-    # )
-
-    # test
+    test
