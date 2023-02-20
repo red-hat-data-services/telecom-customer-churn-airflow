7c6e20830e3dc540106346feff30704c207edda0
RHRolun <rlundber@redhat.com>
2023-02-20 13:16:37 +0100

    Cleanup

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,8 +1,5 @@
 from airflow import DAG
-from airflow.decorators import task
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.providers.papermill.operators.papermill import PapermillOperator
-from airflow.operators.python import PythonOperator
 
 from datetime import datetime, timedelta
 from textwrap import dedent
@@ -19,23 +16,14 @@ with DAG(
     start_date=datetime(2021, 1, 1),
     catchup=False,
 ) as dag:
-    run_this = PapermillOperator(
-        task_id="run_example_notebook",
-        input_nb="test.ipynb",
-        output_nb="/tmp/out-{{ execution_date }}.ipynb",
-        parameters={"msgs": "Ran from Airflow at {{ execution_date }}!"},
+    test = KubernetesPodOperator(
+        namespace="airflow",
+        image="quay.io/eformat/airflow-runner:2.5.1",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10", "echo pwd"],
+        name="test",
+        task_id="test",
+        get_logs=True,
     )
 
-    run_this
-
-    # test = KubernetesPodOperator(
-    #     namespace="airflow",
-    #     image="quay.io/eformat/airflow-runner:2.5.1",
-    #     cmds=["bash", "-cx"],
-    #     arguments=["echo", "10", "echo pwd"],
-    #     name="test",
-    #     task_id="test",
-    #     get_logs=True,
-    # )
-
-    # test
+    test
